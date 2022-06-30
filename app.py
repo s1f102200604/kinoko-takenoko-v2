fed000a742b81a56b486f7225fba7b1bd9087681
s1f102200604 <s1f102200604@iniad.org>
2022-06-30 20:45:28 +0900

    answer関数の変更

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 kinoko_count = 3
 takenoko_count = 5
-message=['Hi1','Hi2']
+messages=['Hi1','Hi2']
 
 @app.route('/')
 def top():
@@ -12,6 +12,12 @@ def top():
 
 @app.route('/vote', methods=['POST'])
 def answer():
+    #投票による更新
+    global kinoko_count, takenoko_count, messages
+    if request.form.get('item')=='kinoko':
+        kinoko_count += 1
+    elif request.form.get('item')=='takenoko':
+        takenoko_count += 1
     #進捗グラフの更新
     kinoko_percent = kinoko_count/(kinoko_count+takenoko_count)*100
     takenoko_percent = takenoko_count/(kinoko_count+takenoko_count)*100
